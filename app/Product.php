9317fbd512b30f57e15701a9974c85840325da13
emiliano <emiliano.0218@gmail.com>
2020-05-09 20:50:42 -0500

    agrego relaciones entre modelos

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,40 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Category;
+use App\Seller;
+use App\Transaction;
 
 class Product extends Model
 {
-    //
+    const PRODUCTO_DISPONIBLE = 'disponible';
+    const PRODUCTO_NO_DISPONIBLE = 'no disponible';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function estaDisponible(){
+        return $this->status == Product::PRODUCTO_DISPONIBLE;
+    }
+
+    //Un producto pertenece a un vendedor
+    public function seller(){
+        return $this->belongsTo(Seller::class);
+    }
+
+    //Un producto tiene muchas transactions
+    public function transactions(){
+        return $this->hasMany(Transaction::class);
+    }
+
+    //Muchos a muchos
+    public function products(){
+        return $this->belongsToMany(Category::class);
+    }
 }
--- app/Seller.php
+++ app/Seller.php
@@ -2,7 +2,12 @@
 
 namespace App;
 
+use App\Product;
+
 class Seller extends User
 {
-    //
+    //un vendedor tiene muchos productos
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
